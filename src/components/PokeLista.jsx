a71f359ec100f01fbb0ab168a957a5050fcf6edb
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-05-20 15:56:06 -0300

    atualizado

--- src/components/PokeLista.jsx
+++ src/components/PokeLista.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router";
 export default function PokeLista({ pokeList }) {
   const navigate = useNavigate();
 
-  function handleDirecionaDetalhes(id) {
-    navigate(`/detalhes/${id}`);
+  function handleDirecionaDetalhes(id, pokeName) {
+    navigate(`/detalhes/${id}?name=${pokeName}`);
   }
 
   return (
@@ -12,7 +12,7 @@ export default function PokeLista({ pokeList }) {
       {pokeList.map((item, index) => (
         <ul key={index}>
           <li>Nome: {item.name}</li>
-          <button onClick={() => handleDirecionaDetalhes(index + 1)}>
+          <button onClick={() => handleDirecionaDetalhes(index + 1, item.name)}>
             Ver Detalhes
           </button>
         </ul>
--- src/pages/DetalhesPage.jsx
+++ src/pages/DetalhesPage.jsx
@@ -1,7 +1,16 @@
+import { useParams, useSearchParams } from "react-router";
+
 export default function DetalhesPage() {
+  const { id } = useParams();
+
+  const [searchParams] = useSearchParams();
+  const name = searchParams.get("name");
+
   return (
     <div>
       <h1>Meu componente pagina de detalhes</h1>
+      <p>meu id: {id}</p>
+      <p>Nome do pokemon: {name}</p>
     </div>
   );
 }
