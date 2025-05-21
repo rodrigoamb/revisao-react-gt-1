be5c9c774ab49c9f0063fbc3cd3aba657ffd4190
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-05-21 13:57:55 -0300

    atualizado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="bg-gray-800 text-white text-center py-4">
+        <p className="text-sm">© 2025 Pokedex. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-black text-3xl font-bold flex flex-row justify-between items-center px-10 py-8">
+      <h1>LOGO</h1>
+      <p>Pokedex</p>
+    </header>
+  );
+}
